7c2e69c6c80b9f69f1680e98db9d0d7b2729cc9c
= <=>
2022-09-17 15:16:12 -0300

    V2

--- intro-react/src/AppUI.js
+++ intro-react/src/AppUI.js
@@ -1,36 +1,41 @@
 import React from "react";
+import {TodoContext} from "./TodoContext";
 import TodoCounter from "./components/TodoCounter.jsx";
 import TodoSearch from "./components/TodoSearch.jsx";
 import TodoList from "./components/TodoList.jsx";
 import CreateTodoButton from "./components/CreateTodoButton.jsx";
 import TodoItem from "./components/TodoItem";
 
-function AppUI({
-  totalTodos,
-  completedTodos,
-  search,
-  setSearch,
-  searchedTodos,
-  completeTodo,
-  deleteTodo,
-}) {
+
+function AppUI() {
   return (
     <>
-      <TodoCounter total={totalTodos} completed={completedTodos} />
+      <TodoCounter/>
+      <TodoSearch/>
 
-      <TodoSearch search={search} setSearch={setSearch} />
-
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
+      <TodoContext.Consumer>
+        {({error,
+         loading, 
+         searchedTodos,
+         completeTodo, 
+        deleteTodo}) => (
+           <TodoList>
+           {error && <p>Hubo un error</p>}
+           {loading && <p>Estamos cargando</p>}
+           {(!loading && !searchedTodos.length) && <p>Crea tu primer ToDo</p>}
+   
+           {searchedTodos.map((todo) => (
+             <TodoItem
+               key={todo.text}
+               text={todo.text}
+               completed={todo.completed}
+               onComplete={() => completeTodo(todo.text)}
+               onDelete={() => deleteTodo(todo.text)}
+             />
+           ))}
+         </TodoList>
+        )}
+      </TodoContext.Consumer>
 
       <CreateTodoButton />
     </>
